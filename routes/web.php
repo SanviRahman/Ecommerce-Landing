04fedd2b6cea19f3571e679e250606c1c62893b4
Sheikh Forid Ahmed Shanto <sheikh15-3700@diu.edu.bd>
2026-05-03 11:03:14 +0600

    Project setup,Spatie Media, Spatie permission, adminlte all setup completed

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Admin Auth routes (login/logout) under /admin prefix
+// Route::prefix('admin')->name('admin.')->middleware('lte_context:admin')->group(function () {
+//     Auth::routes(['register' => false]);
+//     require __DIR__.'/admin.php';
+// });
