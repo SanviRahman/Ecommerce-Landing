2bf608b9272153b0419a9b77995bda6b92cf1608
Sheikh Forid Ahmed Shanto <sheikh15-3700@diu.edu.bd>
2026-08-09 17:19:28 +0600

    update

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -293,6 +293,7 @@
               method="GET"
               action="{{ url()->current() }}">
             <input type="hidden" id="filter_per_page" name="per_page" value="{{ request('per_page', 20) }}">
+            <div class="row">
                 <div class="col-md-2 col-sm-6 mb-2">
                     <label class="small font-weight-bold text-muted text-uppercase">Order Status</label>
                     <select id="filter_order_status"
